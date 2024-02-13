08c0f800d71ed1bca5df1f744d29e8c7f6e4da00
Rutuja Rathod <2021bec070@sggs.ac.in>
2024-02-13 22:19:31 +0530

    new project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <h2>Rutuja Prem Rathod   
+      </h2>
+    </>
+  )
+}
+
+export default App;
